08899329320be63b4ba71f90e42d997aacaa7449
Clipstone <declandavidwww@gmail.com>
2025-11-12 05:33:57 +0000

    Add header styles and pulse animation for player count
    Introduced new header styles in Malevolent.css and added a pulse animation in animations.css for the player count indicator. Updated head.blade.php to include the new animation stylesheet and enhanced header.blade.php with a styled header section displaying server information and online player count.

--- MalevolentWebsite/resources/views/components/global/head.blade.php
+++ MalevolentWebsite/resources/views/components/global/head.blade.php
@@ -4,6 +4,7 @@
 <title>{{ config('app.name', 'Laravel') }} - Homepage</title>
 
 <link rel="stylesheet" href="{{ asset('Malevolent/css/plugins/fontawesome.css') }}"/>
+<link rel="stylesheet" href="{{ asset('Malevolent/css/animations.css') }}"/>
 <link rel="stylesheet" href="{{ asset('Malevolent/css/default.css') }}"/>
 <link rel="stylesheet" href="{{ asset('Malevolent/css/Malevolent.css') }}"/>
 
--- MalevolentWebsite/resources/views/components/global/header.blade.php
+++ MalevolentWebsite/resources/views/components/global/header.blade.php
@@ -1,3 +1,15 @@
 <header>
-
+    <div class="container">
+        <div class="header">
+            <h1>Malevolent Zombie Servers</h1>
+            <p>
+                Welcome to Malevolent zombie servers, at Malevolent servers we host a variety of game modes
+                <br>including gambling servers, high round servers, modded servers and vanilla
+                <br>servers for you to enjoy by yourself or with your friends!
+            </p>
+            <div>
+                <i class="fa-solid fa-circle pulse"></i> 100 players online
+            </div>
+        </div>
+    </div>
 </header>
